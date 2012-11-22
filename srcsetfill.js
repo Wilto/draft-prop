234890fc3f8675644bd428a3543fa55f36ce2272
Marcos Caceres <marcosscaceres@gmail.com>
2012-11-22 12:36:34 +0000

    Started working on responsive model

--- srcsetfill.js
+++ srcsetfill.js
@@ -1,7 +1,6 @@
 /**
 * TODO: need a Viewport object to facilitate testing.
 **/
-
 (function(exports, window) {
     'use strict';
     //The HTML contains definitions/algorithms from HTML5
@@ -200,6 +199,7 @@
     function parseFloat(input) {
         exports.console.warn('Using ECMAScript parse float, as the HTML one is not done yet');
         return window.parseFloat(input);
+        /*
         //Let position be a pointer into input, initially pointing at the start of the string.
         var position = 0,
             value = 1, //Let value have the value 1.
@@ -294,6 +294,7 @@
         //Let rounded-value be the number in S that is closest to value, selecting the number with an even significand if there are two equally close values. (The two special values 21024 and âˆ’21024 are considered to have even significands for this purpose.)
         //If rounded-value is 21024 or âˆ’21024, return an error.
         //Return rounded-value.
+        */
     }
 
     function parseSrcset(attr) {
@@ -551,175 +552,50 @@
         }
         return true;
     }
+
+    // //Asynchronously await a stable state. The synchronous section consists of all the remaining steps of this algorithm until the algorithm says the synchronous section has ended. (Steps in synchronous sections are marked with ⌛.)
+    window.addEventListener("load", function(elem){
+        watchForChange(elem);
+    });
+
+    function watchForChange(elem){
+        //⌛ If the img element does not have a srcset attribute specified,
+        //is not in the completely available state,
+        // has image data whose resource type is multipart/x-mixed-replace,
+        //or if its update the image data algorithm is running, then abort this algorithm.
+        
+        //⌛ Let selected source and selected pixel density be the URL and pixel density
+        //that results from processing the image candidates, respectively.
+        var result= parseSrcset(elem.getAttributeNode("srcset")),
+            selectedSource = result.url,
+            selectedDensity = result.density,
+            CORSMode;
+
+        //⌛ If selected source is null, then abort these steps.
+        if(selectedSource === null){
+            return;
+        }
+
+        //⌛ If selected source and selected pixel density are the same as
+        //the element's last selected source and current pixel density,
+        //then abort these steps.
+
+        //⌛ Resolve selected source, relative to the element.
+
+        //⌛ Let CORS mode be the state of the element's crossorigin content attribute.
+        CORSMode = elem.getAttribute("crossorigin"); 
+
+        //⌛ If the resolve a URL algorithm is not successful, abort these steps.
+
+        //End the synchronous section, continuing the remaining steps asynchronously.
+
+        //Do a potentially CORS-enabled fetch of the resulting absolute URL, with the mode being CORS mode, the origin being the origin of the img element's Document, and the default origin behaviour set to taint.
+
+        //If this download fails in any way (other than the response code not being a 2xx code, as mentioned earlier), or if the image format is unsupported (as determined by applying the image sniffing rules, again as mentioned earlier), or if the resource type is multipart/x-mixed-replace, then abort these steps.
+
+        //Otherwise, wait for the fetch algorithm to queue its last task, and then continue with these steps. The data obtained in this way is used in the steps below.
+
+        //Queue a task to run the following substeps:
+    }
 }(this, window));
-
-
-test(function() {
-    var img = new Image(''),
-        result;
-    img.setAttribute('srcset', '');
-    result = window.srcsetParser.parse(img.getAttributeNode('srcset'));
-    assert_equals(result.url, null);
-    assert_equals(result.density, undefined);
-}, 'If candidates is empty, return null and undefined.');
-
-test(function() {
-    var img = new Image(''),
-        result;
-    img.setAttribute('srcset', '\t \t\r\f\f\r \n \t');
-    result = window.srcsetParser.parse(img.getAttributeNode('srcset'));
-    assert_equals(result.url, null);
-    assert_equals(result.density, undefined);
-}, 'Ignore whitespace at the attribute value.');
-
-
-test(function() {
-    var img = new Image(''),
-        result;
-    img.setAttribute('srcset', 'pass 1x, fail 1x');
-    result = window.srcsetParser.parse(img.getAttributeNode('srcset'));
-    assert_equals(result.url, 'pass');
-    assert_equals(result.density, 1);
-}, 'Ignore duplicates, even if the url is different.');
-
-test(function() {
-    var img = new Image(''),
-        result;
-    img.setAttribute('srcset', 'pass 0002.000x, b 02x, b 02.0000x, fail 2x');
-    result = window.srcsetParser.parse(img.getAttributeNode('srcset'));
-    assert_equals(result.url, 'pass');
-    assert_equals(result.density, 2);
-}, 'Ignore a long list of duplicates with different densities.');
-
-test(function() {
-    var img = new Image(''),
-        result;
-    img.setAttribute('srcset', 'pass 0002.000h, a 02h, c 02.0000h, fail 2h');
-    result = window.srcsetParser.parse(img.getAttributeNode('srcset'));
-    assert_equals(result.url, 'pass');
-    assert_equals(result.density, 1);
-}, 'Ignore a long list of duplicates with the same heights.');
-
-test(function() {
-    var img = new Image(''),
-        result;
-    img.setAttribute('srcset', 'pass 0002.000h 01w 1x, a 02h 001w, c 02.0000h, fail 2h 001w 1x');
-    result = window.srcsetParser.parse(img.getAttributeNode('srcset'));
-    assert_equals(result.url, 'pass');
-    assert_equals(result.density, 1);
-}, 'Ignore a long list of duplicates with the same width and height and density.');
-
-test(function() {
-    var img = new Image(''),
-        result;
-    img.setAttribute('srcset', 'fail 0002h 01w 1x, a 02h 001w 1x, c 02h 0001w 1x, fail 2h 001w 1x, pass 2000w');
-    result = window.srcsetParser.parse(img.getAttributeNode('srcset'));
-    assert_equals(result.url, 'pass');
-    assert_equals(result.density, 1);
-}, 'Ignore a long list of duplicates with the same width and height and density.');
-
-test(function() {
-    var img = new Image(''),
-        result;
-    img.setAttribute('srcset', 'fail 0002h 01w 1x, a 02h 001w 1x, c 02h 0001w 1x, fail 2h 001w 1x, pass 2000w');
-    result = window.srcsetParser.parse(img.getAttributeNode('srcset'));
-    assert_equals(result.url, 'pass');
-    assert_equals(result.density, 1);
-}, 'Ignore a long list of duplicates with the same width and height and density.');
-
-
-test(function() {
-    var img = new Image(''),
-        result;
-    img.setAttribute('src', 'pass');
-    img.setAttribute('srcset', 'fail 1w, fail 2w, fail 3w');
-    result = window.srcsetParser.parse(img.getAttributeNode('srcset'));
-    assert_equals(result.url, 'pass');
-    assert_equals(result.density, 1);
-}, 'Test that src attribute is selected as a candidate.');
-
-test(function() {
-    var img = new Image(''),
-        result;
-    img.setAttribute('src', 'fail_src');
-    img.setAttribute('srcset', 'fail1 1w, pass 2000w, fail2 2000w, fail3 3w');
-    result = window.srcsetParser.parse(img.getAttributeNode('srcset'));
-    assert_equals(result.url, 'pass');
-    assert_equals(result.density, 1);
-}, 'Test that src attribute is not selected as a candidate.');
-
-test(function() {
-    var img = new Image(''),
-        result;
-    img.setAttribute('src', 'fail_src');
-    img.setAttribute('srcset', 'pass 2000w, fail1 3000w, fail2 4000w');
-    result = window.srcsetParser.parse(img.getAttributeNode('srcset'));
-    assert_equals(result.url, 'pass');
-    assert_equals(result.density, 1);
-}, 'Test selection of smallest large width.');
-
-test(function() {
-    var img = new Image(''),
-        result;
-    img.setAttribute('src', 'fail_src');
-    img.setAttribute('srcset', 'pass 2000h, fail1 3000h, fail2 4000h');
-    result = window.srcsetParser.parse(img.getAttributeNode('srcset'));
-    assert_equals(result.url, 'pass');
-    assert_equals(result.density, 1);
-}, 'Test selection of smallest large height.');
-
-
-test(function() {
-    var img = new Image(''),
-        result;
-    img.setAttribute('src', 'fail_src');
-    img.setAttribute('srcset', 'fail1 3000h, fail2 4000h, pass 2000h');
-    result = window.srcsetParser.parse(img.getAttributeNode('srcset'));
-    assert_equals(result.url, 'pass');
-    assert_equals(result.density, 1);
-}, 'Test selection of smallest large height.');
-
-test(function() {
-    var img = new Image(''),
-        result;
-    img.setAttribute('src', 'fail_src');
-    img.setAttribute('srcset', 'fail1 3000h, fail2 4000h, pass 2000h, fail1 3000h, fail 2000h, fail2 4000h');
-    result = window.srcsetParser.parse(img.getAttributeNode('srcset'));
-    assert_equals(result.url, 'pass');
-    assert_equals(result.density, 1);
-}, 'Test selection of smallest large height, with redudant values.');
-
-test(function() {
-    var img = new Image(''),
-        result,
-        width = window.innerWidth + 'w';
-    img.setAttribute('src', 'fail_src');
-    img.setAttribute('srcset', 'pass ' + width, 'fail ' + width);
-    result = window.srcsetParser.parse(img.getAttributeNode('srcset'));
-    assert_equals(result.url, 'pass');
-    assert_equals(result.density, 1);
-}, "Test selection of the right source when window's width is the same as the source.");
-
-test(function() {
-    var img = new Image(''),
-        result,
-        height = window.innerHeight;
-    img.setAttribute('src', 'fail_src');
-    img.setAttribute('srcset', 'pass ' + height + 'h', 'fail ' + (height - 1) + 'h', 'fail ' + (height + 1) + 'h');
-    result = window.srcsetParser.parse(img.getAttributeNode('srcset'));
-    assert_equals(result.url, 'pass');
-    assert_equals(result.density, 1);
-}, "Test selection of the right source when window's width is the same as the source and other bigger and smaller sources are available.");
-
-
-test(function() {
-    var img = new Image(''),
-        result,
-        height = window.innerHeight;
-    img.setAttribute('src', 'fail_src');
-    img.setAttribute('srcset', 'pass ' + height + 'h', 'fail ' + (height - 1) + 'h', 'fail ' + (height + 1) + 'h');
-    result = window.srcsetParser.parse(img.getAttributeNode('srcset'));
-    assert_equals(result.url, 'pass');
-    assert_equals(result.density, 1);
-}, "Test selection of the right source when window's height is the same as the source and other bigger and smaller sources are available.");
 
