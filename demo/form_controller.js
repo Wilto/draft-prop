4395b430c2301f0f8f339d13fe785a7763e729ed
Marcos Caceres <marcos.caceres@wacapps.net>
2012-11-27 09:21:44 +0000

    fixed style

--- demo/form_controller.js
+++ demo/form_controller.js
@@ -1,44 +1,44 @@
-(function (window) {
-    "use strict";
-    window.addEventListener("DOMContentLoaded", setUpTester);
+(function(window) {
+    'use strict';
+    window.addEventListener('DOMContentLoaded', setUpTester);
 
     function setUpTester() {
-        var srcsetTester = document.getElementById("srcsetTester"),
-            elem = document.createElement("x-element"),
+        var srcsetTester = document.getElementById('srcsetTester'),
+            elem = document.createElement('x-element'),
             attrValues = [{
-                name: "srcset",
-                value: ""
+                name: 'srcset',
+                value: ''
             }, {
-                name: "src",
-                value: "default.png"
+                name: 'src',
+                value: 'default.png'
             }],
             srcsetAttr;
         //create fake IDL attributes for x-element
-        attrValues.forEach((function () {
-            return function (prop) {
+        attrValues.forEach((function() {
+            return function(prop) {
                 setupAttributes(prop);
                 //initialize attribute
                 elem[prop.name] = prop.value;
-            }
+            };
         }()));
-        srcsetAttr = elem.getAttributeNode("srcset");
+        srcsetAttr = elem.getAttributeNode('srcset');
         //Wire up events
-        window.addEventListener("resize", popualteForm)
-        srcsetTester.addEventListener("input", findSrcset);
+        window.addEventListener('resize', popualteForm);
+        srcsetTester.addEventListener('input', findSrcset);
         //run
         popualteForm();
 
         function setupAttributes(prop) {
             var props = {
-                get: function () {
+                get: function() {
                     return prop.value;
                 },
-                set: function (value) {
+                set: function(value) {
                     elem.setAttribute(prop.name, String(value));
                 }
-            }
+            };
             Object.defineProperty(elem, prop.name, props);
-            srcsetTester[prop.name].addEventListener("keyup", function (e) {
+            srcsetTester[prop.name].addEventListener('keyup', function(e) {
                 elem[prop.name] = e.target.value;
             });
         }
@@ -51,7 +51,7 @@
         }
 
         function findSrcset() {
-            var result = window.srcsetParser.parse(srcsetAttr)
+            var result = window.srcsetParser.parse(srcsetAttr);
             showResult(result);
         }
 
