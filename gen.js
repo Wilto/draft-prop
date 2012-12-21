9c5a15331fe2ce6e29c0eca5cb50dfb346e1d55c
Marcos Caceres <marcos.caceres@wacapps.net>
2012-12-21 18:16:37 +0000

    removed tabs from gen script

--- gen.js
+++ gen.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
 var sys = require('sys'),
-	fs = require("fs"),
-	path = require('path'),
+    fs = require("fs"),
+    path = require('path'),
     exec = require('child_process').exec,
     months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-	today = new Date(),
-	pubdate = today.getDate() + ' ' + months[today.getMonth()] + ' ' + today.getFullYear(),
-	filter = '.w3c',
-	shortName = path.basename(process.cwd()),
-	output = 'index.html',
-	pubDir = process.cwd() + '/pub',
+    today = new Date(),
+    pubdate = today.getDate() + ' ' + months[today.getMonth()] + ' ' + today.getFullYear(),
+    filter = '.w3c',
+    shortName = path.basename(process.cwd()),
+    output = 'index.html',
+    pubDir = process.cwd() + '/pub',
     argv = require('optimist')
-           .usage('Usage: $0 [-w3c] [-pubdate \"dd Mon yyy\"] [-type ED|WD|CR|PR|REC]')
+           .usage('Usage: $0 [--w3c] [--pubdate \"dd Mon yyy\"] [--type ED|WD|CR|PR|REC]')
            .default('pubdate', pubdate)
            .default('type', 'ED')
            .default('name', shortName)
@@ -19,21 +19,21 @@ var sys = require('sys'),
     child, anolis;
 
 if (argv.hasOwnProperty('w3c') && argv.w3c) {
-	//default to Working Draft
-	if (argv.type === 'ED') {
-		argv.type = 'WD';
-	}
+    //default to Working Draft
+    if (argv.type === 'ED') {
+        argv.type = 'WD';
+    }
 
-	//Take out all RICG stuff if we are targetting W3C
-	filter = '.ricg';
+    //Take out all RICG stuff if we are targetting W3C
+    filter = '.ricg';
 
-	//e.g., "WD_21Dec2012.html"
-	output = 'pub/' + argv.type + '_' + argv.pubdate.split(' ').join('') + '.html';
+    //e.g., "WD_21Dec2012.html"
+    output = 'pub/' + argv.type + '_' + argv.pubdate.split(' ').join('') + '.html';
 
-	//check that we have a pub directory
-	if(fs.existsSync(pubDir) === false){
-		fs.mkdirSync(pubDir);
-	}
+    //check that we have a pub directory
+    if(fs.existsSync(pubDir) === false){
+        fs.mkdirSync(pubDir);
+    }
 }
 
 anolis = 'anolis --w3c-compat --output-encoding=utf8' +
@@ -45,27 +45,27 @@ anolis = 'anolis --w3c-compat --output-encoding=utf8' +
          ' index.src.html ' + output;
 
 exec(anolis, function(error, stdout, stderr) {
-	if (error) {
-		console.log('Something went wrong', error, stdout, stderr);
-		return;
-	}
-	showNotification();
+    if (error) {
+        console.log('Something went wrong', error, stdout, stderr);
+        return;
+    }
+    showNotification();
 });
 
 function showNotification() {
-	var hasNotifier = 'command -v terminal-notifier >/dev/null 2>&1';
-	exec(hasNotifier, function(error, stdout, stderr) {
-		var command;
-		//No notifier, so abort
-		if (error !== null) {
-			console.log('Missing optional dependency: sudo gem install terminal-notifier');
-			return;
-		}
-		command = 'terminal-notifier '+
+    var hasNotifier = 'command -v terminal-notifier >/dev/null 2>&1';
+    exec(hasNotifier, function(error, stdout, stderr) {
+        var command;
+        //No notifier, so abort
+        if (error !== null) {
+            console.log('Missing optional dependency: sudo gem install terminal-notifier');
+            return;
+        }
+        command = 'terminal-notifier '+
                   '-message \"Finished processing document.\" '+
                   '-title \"Anolis Processing done\" '+
                   '-execute \"open '+ process.cwd() + '/'+ output + '\" '+
                   '-group \"Anolis\"';
     exec(command);
-	});
+    });
 }
